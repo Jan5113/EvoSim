63b45b67f47f7a4d2bbf756fc8816cb96663f69c
Jan <Jan@TERMINATOR>
2017-09-14 22:00:13 +0200

    Added draw functions for "B2DCube"s

--- EvoSim/src/Screen.java
+++ EvoSim/src/Screen.java
@@ -1,15 +1,19 @@
+import org.jbox2d.common.Vec2;
+
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public class Screen extends Canvas {
 	GraphicsContext gc;
+	Color col_background;
 
 	public Screen(double arg0, double arg1) {
 		super(arg0, arg1);
+		
 		gc = this.getGraphicsContext2D();
-		gc.setFill(Color.color(1, 0.8, 0.8));
-		gc.fillRect(0, 0, this.getWidth(), this.getHeight());
+		col_background = Color.color(1, 0.8, 0.8);
+		clearScreen();
 	}
 
 	public void drawCircle(double x, double y, double w, double h, double deg, Color c, boolean fill) { 
@@ -40,6 +44,15 @@ public class Screen extends Canvas {
 	    gc.restore();
 	}
 
+	public void drawRect(Vec2 pos, Vec2 dim, double deg, Color c, boolean fill) {
+	    gc.save();
+	    gc.translate(pos.x, pos.y);
+	    gc.rotate(deg);
+	    gc.translate(-pos.x, -pos.y);
+		drawRect(pos.x, pos.y, dim.x, dim.y, c, fill);
+	    gc.restore();
+	}
+
 	public void drawRect(double x, double y, double w, double h, Color c, boolean fill) {
 		gc.setStroke(c);
 		gc.setFill(c);
@@ -49,6 +62,15 @@ public class Screen extends Canvas {
 		else
 			gc.strokeRect(x - w, y - h, 2*w, 2*h);
 		
+	}
+	
+	public void drawRect(B2DCube b2d_cube, Color c, boolean fill) {
+		drawRect(ConvertUnits.coordWorldToPixels(b2d_cube.getPos()), ConvertUnits.scalarWorldToPixels(b2d_cube.getDim()), ConvertUnits.radToDeg(-b2d_cube.getRot()), c, fill);
+	}
+	
+	public void clearScreen () {		
+		gc.setFill(col_background);
+		gc.fillRect(0, 0, this.getWidth(), this.getHeight());
 	}
 
 }
