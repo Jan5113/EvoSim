46fdda46030ba8fcdae004b422d25912b62893de
Jan <Jan@TERMINATOR>
2017-09-15 19:34:34 +0200

    Played around... LHC!

--- EvoSim/src/Main.java
+++ EvoSim/src/Main.java
@@ -22,9 +22,11 @@ public class Main extends Application{
 	
 	private World world;
 	private long lastNanoTime;
-	private static final Vec2 v2_gravity = new Vec2(0, -10);
+	private static final Vec2 v2_gravity = new Vec2(0, -0);
 	private ArrayList<B2DCube> al_cubes = new ArrayList<B2DCube>();
 	private Vec2 dir = new Vec2(0,0);
+	private Vec2 shootDir;
+	private Vec2 mousePos;
 	
 	public static void main(String[] args) {
 		launch (args);
@@ -38,9 +40,12 @@ public class Main extends Application{
 		scene.setFill(Color.color(0.8, 0.8, 1));
 		
 		primaryStage.setScene(scene);
-		screen = new Screen(500, 500, 100, new Vec2(0, 5));
-		screen.setOnMouseClicked(e -> onClickScreen(e));
-		screen.setOnMouseMoved(e -> onPressedScreen(e));
+		screen = new Screen(500, 500, 100, new Vec2(0, 2.4f));
+		screen.setBackgroundCol(Color.ALICEBLUE);
+		screen.setOnMouseReleased(e -> onClickScreen(e));
+		screen.setOnMouseMoved(e -> onMoveScreen(e));
+		screen.setOnMousePressed(e -> onPressedScreen(e));
+		screen.setOnMouseDragged(e -> onEnteredScreen(e));
 		scene.addEventHandler(KeyEvent.KEY_PRESSED, e -> onKeyScreen(e));
 		scene.addEventHandler(KeyEvent.KEY_RELEASED, e -> offKeyScreen(e));
 		
@@ -52,7 +57,7 @@ public class Main extends Application{
 		primaryStage.widthProperty().addListener((obs, old, nev) -> stageResize(primaryStage));
 		
 		world = new World(v2_gravity);
-		al_cubes.add(new B2DCube(2.5f, 0.5f, 2.0f, 0.1f, BodyType.STATIC, world));	
+		al_cubes.add(new B2DCube(0f, 0.2f, 2.0f, 0.1f, BodyType.STATIC, world));	
 		
 		final long startNanoTime = System.nanoTime();
 	    lastNanoTime = startNanoTime;
@@ -68,7 +73,7 @@ public class Main extends Application{
 	          }
 	      }.start();
 	}
-	
+
 	private void offKeyScreen(KeyEvent e) {
 		if (e.getCode() == KeyCode.W) {
 			dir.y = 0;
@@ -104,28 +109,59 @@ public class Main extends Application{
 		if (e.getCode() == KeyCode.E) {
 			screen.setScale(screen.getScale()*1.25);
 		}
+		if (e.getCode() == KeyCode.SPACE) {	
+			for (int i = 0; i < 50; i++) {
+				al_cubes.add(new B2DCube(mousePos,
+						new Vec2(0.1f * (float)Math.random() + 0.05f, 0.1f* (float)Math.random() + 0.05f),
+						new Vec2((float) Math.random() - 0.5f, (float) Math.random() - 0.5f).mul(1.0f), (float) (Math.random()*Math.PI),
+						BodyType.DYNAMIC, world));
+				
+			}
+		}
 		dir.normalize();
 	}
 
 	private void refreshScreen(double dt) {
-		screen.clearScreen();
 		world.step((float) dt, 10, 10);
+		screen.addPos(dir.mul((float) (dt * 1000/screen.getScale())));
+		screen.clearScreen();
 		
 		for (B2DCube c : al_cubes) {
 			screen.drawRect(c, Color.BLUE, false);
 		}
 		
-		screen.addPos(dir.mul((float) dt));
-	}
-
-	private void onClickScreen(MouseEvent e) {
-		//al_cubes.add(new B2DCube(ConvertUnits.coordPixelsToWorld(e.getX(), e.getY()), new Vec2(0.1f, 0.1f), BodyType.DYNAMIC, world));
+		if (shootDir != null && mousePos != null) {
+			screen.drawLine(shootDir, mousePos, Color.RED);
+		}
+		
 	}
 	
 	private void onPressedScreen(MouseEvent e) {
-		al_cubes.add(new B2DCube(screen.cu.coordPixelsToWorld(e.getX(), e.getY()),
-				new Vec2(0.1f * (float)Math.random() + 0.05f, 0.1f* (float)Math.random() + 0.05f),
+		mousePos = screen.cu.coordPixelsToWorld(e.getX(), e.getY());	
+		shootDir = screen.cu.coordPixelsToWorld(e.getX(), e.getY());
+	}
+
+	private void onClickScreen(MouseEvent e) {
+		mousePos = screen.cu.coordPixelsToWorld(e.getX(), e.getY());	
+		//al_cubes.add(new B2DCube(ConvertUnits.coordPixelsToWorld(e.getX(), e.getY()), new Vec2(0.1f, 0.1f), BodyType.DYNAMIC, world));
+		Vec2 shoot_vel = shootDir.add(screen.cu.coordPixelsToWorld(e.getX(), e.getY()).negate()).mul(5.0f);
+		al_cubes.add(new B2DCube(shootDir,
+				new Vec2(2f, 2f),
+				shoot_vel, (float) (Math.random()*Math.PI),
 				BodyType.DYNAMIC, world));
+		shootDir = null;
+	}
+	
+	private void onMoveScreen(MouseEvent e) {	
+		mousePos = screen.cu.coordPixelsToWorld(e.getX(), e.getY());	
+//		al_cubes.add(new B2DCube(screen.cu.coordPixelsToWorld(e.getX(), e.getY()),
+//				new Vec2(0.1f * (float)Math.random() + 0.05f, 0.1f* (float)Math.random() + 0.05f),
+//				new Vec2((float) Math.random() - 0.5f, (float) Math.random() - 0.5f).mul(10.0f), (float) (Math.random()*Math.PI),
+//				BodyType.DYNAMIC, world));
+	}
+	
+	private void onEnteredScreen(MouseEvent e) {
+		mousePos = screen.cu.coordPixelsToWorld(e.getX(), e.getY());	
 	}
 
 	private void stageResize(Stage s) {
