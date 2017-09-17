db2740e5adf56c4c90e5f73fb0c70355be5a2f2e
Jan <Jan@TERMINATOR>
2017-09-18 00:17:33 +0200

    Added functions to draw B2DSpheres

--- EvoSim/src/Screen.java
+++ EvoSim/src/Screen.java
@@ -23,16 +23,27 @@ public class Screen extends Canvas {
 	}
 
 	@SuppressWarnings("unused")
-	private void drawCircle(double x, double y, double w, double h, double deg, Color c, boolean fill) {
+	private void drawPxCircle(double x, double y, double w, double h, double deg, Color c, boolean fill) {
 		gc.save();
 		gc.translate(x, y);
 		gc.rotate(deg);
 		gc.translate(-x, -y);
-		drawCircle(x, y, w, h, c, fill);
+		drawPxCircle(x, y, w, h, c, fill);
+		gc.restore();
+	}
+	
+	private void drawPxLineCircle(double x, double y, double r, double deg, Color c, boolean fill) {
+		gc.save();
+		gc.translate(x, y);
+		gc.rotate(deg);
+		gc.translate(-x, -y);
+		drawPxCircle(x, y, r, r, c, fill);
+		gc.setStroke(c);
+		gc.strokeLine(x, y, x + r, y);
 		gc.restore();
 	}
 
-	private void drawCircle(double x, double y, double w, double h, Color c, boolean fill) {
+	private void drawPxCircle(double x, double y, double w, double h, Color c, boolean fill) {
 		gc.setStroke(c);
 		gc.setFill(c);
 
@@ -43,25 +54,25 @@ public class Screen extends Canvas {
 	}
 
 	@SuppressWarnings("unused")
-	private void drawRect(double x, double y, double w, double h, double deg, Color c, boolean fill) {
+	private void drawPxRect(double x, double y, double w, double h, double deg, Color c, boolean fill) {
 		gc.save();
 		gc.translate(x, y);
 		gc.rotate(deg);
 		gc.translate(-x, -y);
-		drawRect(x, y, w, h, c, fill);
+		drawPxRect(x, y, w, h, c, fill);
 		gc.restore();
 	}
 
-	private void drawRect(Vec2 pos, Vec2 dim, double deg, Color c, boolean fill) {
+	private void drawPxRect(Vec2 pos, Vec2 dim, double deg, Color c, boolean fill) {
 		gc.save();
 		gc.translate(pos.x, pos.y);
 		gc.rotate(deg);
 		gc.translate(-pos.x, -pos.y);
-		drawRect(pos.x, pos.y, dim.x, dim.y, c, fill);
+		drawPxRect(pos.x, pos.y, dim.x, dim.y, c, fill);
 		gc.restore();
 	}
 
-	private void drawRect(double x, double y, double w, double h, Color c, boolean fill) {
+	private void drawPxRect(double x, double y, double w, double h, Color c, boolean fill) {
 		gc.setStroke(c);
 		gc.setFill(c);
 
@@ -72,9 +83,16 @@ public class Screen extends Canvas {
 
 	}
 
-	public void drawRect(B2DCube b2d_cube, Color c, boolean fill) {
-		drawRect(cu.coordWorldToPixels(b2d_cube.getPos()), cu.scalarWorldToPixels(b2d_cube.getDim()),
+	public void drawCube(B2DCube b2d_cube, Color c, boolean fill) {
+		drawPxRect(cu.coordWorldToPixels(b2d_cube.getPos()), cu.scalarWorldToPixels(b2d_cube.getDim()),
 				cu.radToDeg(-b2d_cube.getRot()), c, fill);
+	}
+
+	public void drawSphere(B2DSphere b2d_sphere, Color c, boolean fill) {
+		Vec2 pos = cu.coordWorldToPixels(b2d_sphere.getPos());
+		double rad = (double) cu.scalarWorldToPixels(b2d_sphere.getRadius());
+		double angl = cu.radToDeg(-b2d_sphere.getRot());
+		drawPxLineCircle(pos.x, pos.y, rad, angl, c, fill);
 	}
 	
 	public void drawLine(Vec2 b2d_pos1, Vec2 b2d_pos2, Color c) {
