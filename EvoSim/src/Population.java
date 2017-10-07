aa45d9a6887c8dcad6f2b8a3b154f5f676578b1b
Jan <Jan@TERMINATOR>
2017-10-07 10:46:19 +0200

    Added population, holds Creatures()

---
+++ EvoSim/src/Population.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Population {
+	public final int populationSize;
+	private ArrayList<Creature> worldInstancesList = new ArrayList<Creature>();
+	
+	
+	public Population(int popSize_in) {
+		populationSize = popSize_in;
+		worldInstancesList.size();
+	}
+}
