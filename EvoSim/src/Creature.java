f80854f6f0d704272890ca019e2fbf83c5c29bf5
Jan <Jan@TERMINATOR>
2017-10-09 15:56:13 +0200

    Added functions for genetic algorithm

--- EvoSim/src/Creature.java
+++ EvoSim/src/Creature.java
@@ -1,9 +1,9 @@
 import org.jbox2d.common.Vec2;
 
-public class Creature {
-	public static float[] minMaxLength = {0.2f, 1.0f};
-	public static float[] minMaxTime = {0.0f, 0.5f};
-	public static Vec2 ballStartPosition = new Vec2(0.0f, 7.0f);
+public class Creature implements Comparable<Creature>{
+	public static float[] minMaxLength = {0.2f, 2.0f};
+	public static float[] minMaxTime = {0.0f, 1.0f};
+	public static Vec2 ballStartPosition = new Vec2(0.0f, 17.0f);
 	public static Vec2 fixturePosition = new Vec2(-0.5f, 2.0f);
 	public static float ballDim = 0.1f;
 	
@@ -19,11 +19,29 @@ public class Creature {
 		id = id_in;
 	}
 	
+	public Creature(int id_in, float length, float time) {
+		this.length = length;
+		this.time = time;
+		id = id_in;
+	}
+	
 	public void setFitness(float fitness_in) {
 		fitness = fitness_in;
 	}
 	
 	public float getFitness() {
 		return fitness;
+	}
+	
+	public Creature mutate(float amount, int id) {
+		float newLen = this.length * (1 + amount * (2 * (float) Math.random() - 1.0f));
+		float newTime = this.time * (1 + amount * (2 * (float) Math.random() - 1.0f));
+		return new Creature(id, newLen, newTime);
+	}
+
+	public int compareTo(Creature c) {
+		if (this.fitness < c.getFitness()) return 1;
+		else if (this.fitness > c.getFitness()) return -1;
+		return 0;
 	}
 }
