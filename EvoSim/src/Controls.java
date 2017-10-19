5039363b8730359e0969cd8b13ea0470b0327842
Jan <Jan@LAPTOP>
2017-10-19 18:31:56 +0200

    Added new class Controls, MID WORK

---
+++ EvoSim/src/Controls.java
@@ -0,0 +1,20 @@
+import javafx.beans.property.ReadOnlyIntegerProperty;
+import javafx.scene.layout.GridPane;
+
+public class Controls {
+	int clicks = 0;
+	
+	GridPane gp_controls = new GridPane();
+	
+	
+	public Controls() {
+		
+	}
+	
+	public final ReadOnlyIntegerProperty heightProperty() {
+		return null;
+	}
+	
+	
+
+}
