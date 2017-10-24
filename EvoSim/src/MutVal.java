6ba5d8f802e440adfafa210979ddf1853299016c
Jan <Jan@TERMINATOR>
2017-10-24 23:36:35 +0200

    changed mutation-algorithm

--- EvoSim/src/MutVal.java
+++ EvoSim/src/MutVal.java
@@ -4,8 +4,8 @@ public class MutVal {
 	private final float value;
 	private final float mutRange;
 	private Random normDis = new Random();
-	private static float mutFactor = 2.0f;
-	private static float bigMutFactor = 5.0f;
+	private static float minRangeChange = 0.9f; //between 0 and 1
+	private static float bigMutFactor = 10.0f;
 	
 	public MutVal(float min, float max, float rng) {
 		if (min > max) max += (min - (min = max)); // swap
@@ -19,13 +19,15 @@ public class MutVal {
 	}
 	
 	public MutVal mutate() {
-		float diff = (float) normDis.nextGaussian() * mutRange;
+		float mutFactor = (float) normDis.nextGaussian();
+		float diff = mutFactor * mutRange;
+		float rangeChange = Math.abs(mutFactor) + minRangeChange;
 		
 		if (normDis.nextFloat() < 0.05) { // 5% chance for bigger mutation 
 			diff *= bigMutFactor;
 		}
 			
-		return new MutVal(value + diff, diff * mutFactor);
+		return new MutVal(value + diff, diff * rangeChange);
 	}
 	
 	public float getVal() {
