348fb4740ca25746be2d1c5458b3e12cbbec51e1
Jan <Jan@TERMINATOR>
2017-11-26 22:28:29 +0100

    Added API comments for PlayBackControls

--- EvoSim/src/PlayBackControls.java
+++ EvoSim/src/PlayBackControls.java
@@ -3,11 +3,37 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
+/**
+ * The {@link PlayBackControls} class is a {@link BorderPane} which holds all
+ * the buttons. It takes references of the elements it controls such as
+ * {@link TestScreen} instances or {@link MultiTest} instances. All the commands
+ * are then handed over to the correspondent reference.
+ * <p>
+ * <strong>NOTE:</strong> The refresh() method has to be called every frame to
+ * ensure the correct labelling of some "dynamic" buttons.
+ * <p>
+ * This class uses the {@link Layout} class for the way of displaying the
+ * elements.
+ *
+ */
 public class PlayBackControls extends BorderPane {
 
+	/**
+	 * {@code testScreen} is a reference to the {@link TestScreen} instance this
+	 * {@link PlayBackControls} instance has control over. This class communicates
+	 * with the {@code TestScreen} via {@link ControlFuncTest} enums.
+	 */
 	private final TestScreen testScreen;
+	/**
+	 * {@code multiTest} is a reference to the {@link MultiTest} instance this
+	 * {@link PlayBackControls} instance has control over.
+	 */
 	private final MultiTest multiTest;
 
+	/**
+	 * {@code gp_controls} is the {@link GridPane} which holds all the buttons
+	 * and arranges them accordingly.
+	 */
 	GridPane gp_controls = new GridPane();
 
 	// Playback Controls
@@ -20,6 +46,17 @@ public class PlayBackControls extends BorderPane {
 	Button btn_resetView = new Button("Reset View");
 	Button btn_test = new Button("TESTMULTICORE");
 
+	/**
+	 * Initialises the new {@link PlayBackControls} instance with references to the
+	 * elements it controls.
+	 * 
+	 * @param testScreen_in
+	 *            reference to the {@link TestScreen} instance this
+	 *            {@link PlayBackControls} should have control over.
+	 * @param multiTest_in
+	 *            reference to the {@link MultiTest} instance this
+	 *            {@link PlayBackControls} should have control over.
+	 */
 	public PlayBackControls(TestScreen testScreen_in, MultiTest multiTest_in) {
 		testScreen = testScreen_in;
 		multiTest = multiTest_in;
@@ -58,6 +95,11 @@ public class PlayBackControls extends BorderPane {
 		btn_test.setOnAction(e -> testMultiCore());
 	}
 	
+	/**
+	 * Handles the labelling of "dynamic" buttons. Those are buttons which change
+	 * their labelling depending on the status of the elements this
+	 * {@link PlayBackControls} instance has control over.
+	 */
 	public void refresh() {
 		if (testScreen.isTestRunning())
 			btn_playpause.setText("Pause");
