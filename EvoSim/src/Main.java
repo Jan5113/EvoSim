d394b7adf40029c0b07001568dc81d0ed687ccca
Jan <Jan@TERMINATOR>
2017-12-02 17:30:57 +0100

    Added and implemented a overview with all creatures

--- EvoSim/src/Main.java
+++ EvoSim/src/Main.java
@@ -3,23 +3,21 @@ import org.jbox2d.common.Vec2;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application{
-	private static String version = "0.0.1";
-	GraphicsContext gc;
+	private static String version = "0.1.0";
 	
 	private long lastNanoTime;
 	private TestScreen mainTestScreen;
 	private MultiTest mainMultiTest;
 	private PlayBackControls bp_control;
+	private PopScreen popScreen;
 	
-	private Population pop = new Population();
+	private Population pop = new Population(new Vec2(0.0f, -9.81f));
 	
 	public static void main(String[] args) {
 		launch (args);
@@ -33,25 +31,29 @@ public class Main extends Application{
 		scene.setFill(Color.color(0.8, 0.8, 1));
 		
 		primaryStage.setScene(scene);
-		mainTestScreen = new TestScreen(new Vec2(0.0f, -9.81f), 1100, 500, 70, new Vec2(0.0f, 3.0f), pop);
-		mainTestScreen.setBackgroundCol(Color.ALICEBLUE);
+		mainTestScreen = new TestScreen(900, 500, 70, new Vec2(0.0f, 3.0f), pop);
+		mainTestScreen.setBackgroundCol(Layout.getSkycolor());
 		scene.addEventHandler(KeyEvent.KEY_PRESSED, e -> onKeyScreen(e));
 		scene.addEventHandler(KeyEvent.KEY_RELEASED, e -> offKeyScreen(e));
 		
-		mainMultiTest = new MultiTest(6, new Vec2(0.0f, -9.81f), pop);
+		mainMultiTest = new MultiTest(6, pop);
 		
 		bp_control = new PlayBackControls(mainTestScreen, mainMultiTest);
 		mainTestScreen.enableAutoGetNext();
 		mainTestScreen.enableInfo();
 		mainTestScreen.enableMarkers();
+
+		pop.CreateRandPopulation(100);
+		
+		popScreen = new PopScreen(pop, mainTestScreen);
 		
 		root.setBottom(bp_control);
 		root.setCenter(mainTestScreen);
+		root.setLeft(popScreen);
 		
 		
 		primaryStage.show();
 		
-		pop.CreateRandPopulation(100);
 		
 		primaryStage.heightProperty().addListener((obs, old, nev) -> stageResize(primaryStage));
 		primaryStage.widthProperty().addListener((obs, old, nev) -> stageResize(primaryStage));
@@ -76,22 +78,17 @@ public class Main extends Application{
 	}
 
 	private void onKeyScreen(KeyEvent e) {
-		if (e.getCode() == KeyCode.Q) {
-			mainTestScreen.setScale(mainTestScreen.getScale()*0.8f);
-		}
-		if (e.getCode() == KeyCode.E) {
-			mainTestScreen.setScale(mainTestScreen.getScale()*1.2f);
-		}
 	}
 
 	private void refreshScreen(float dt) {
 		mainTestScreen.refresh(dt);
 		bp_control.refresh();
+		popScreen.refresh(dt);
 
 	}
 
 	private void stageResize(Stage s) {
-		mainTestScreen.setScreenSize((int) s.getWidth()-100, (int) s.getHeight()-200); 
+		mainTestScreen.setScreenSize((int) s.getWidth()-570, (int) s.getHeight()-200); 
 	}
 
 }
---
+++ EvoSim/src/PopScreen.java
@@ -0,0 +1,67 @@
+import org.jbox2d.common.Vec2;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.TilePane;
+
+public class PopScreen extends ScrollPane{
+	private TestScreen[] testScreenArr;
+	private Population pop;
+	private TilePane tp_screens = new TilePane();
+	private final TestScreen mainTestScreen;
+	
+	public PopScreen(Population pop_in, TestScreen testScreen_in) {
+		mainTestScreen = testScreen_in;
+		pop = pop_in;
+		testScreenArr = new TestScreen[pop.getPopulationSize()];
+		for (int i = 0; i < testScreenArr.length; i++) {
+			int tileNr = i;
+			testScreenArr[i] = new TestScreen(150, 150, 15, new Vec2(-1,4), pop);
+			testScreenArr[i].addEventHandler(MouseEvent.MOUSE_ENTERED, e -> onMouseEntered(e, tileNr));
+			testScreenArr[i].addEventHandler(MouseEvent.MOUSE_EXITED, e -> onMouseExited(e, tileNr));
+			testScreenArr[i].addEventHandler(MouseEvent.MOUSE_CLICKED, e -> onMouseClicked(e, tileNr));
+			testScreenArr[i].startSingleTest(i);
+			testScreenArr[i].setBackgroundCol(Layout.getSkycolor());
+			testScreenArr[i].manageCommand(ControlFuncTest.STOP);
+			testScreenArr[i].enableAutoRepeat();
+			testScreenArr[i].setFollowOffset(new Vec2(-1, 0));
+			testScreenArr[i].enableCompactInfo();
+			testScreenArr[i].disableScrollZoom();
+			TilePane.setMargin(testScreenArr[i], new Insets(4));
+			tp_screens.getChildren().add(testScreenArr[i]);
+		}
+		tp_screens.setVgap(10);
+		tp_screens.setHgap(10);
+		tp_screens.setPrefColumns(3);
+		
+		this.setPrefWidth(520);
+		this.setContent(tp_screens);
+		this.setStyle("-fx-font-size: 18px;"
+				+ "-fx-focus-color: transparent;"
+				+ "-fx-background-insets: -1.4, 0, 1, 2;");
+	}
+
+	private void onMouseClicked(MouseEvent e, int tileNr) {
+		if(e.getButton().equals(MouseButton.PRIMARY)){
+            if(e.getClickCount() == 2){
+        		mainTestScreen.startSingleTest(tileNr);
+            }
+        }
+	}
+
+	private void onMouseExited(MouseEvent e, int tileNr) {
+		testScreenArr[tileNr].manageCommand(ControlFuncTest.STOP);
+	}
+
+	private void onMouseEntered(MouseEvent e, int tileNr) {
+		testScreenArr[tileNr].manageCommand(ControlFuncTest.START);
+	}
+
+	public void refresh(float dt) {
+		for (int i = 0; i < testScreenArr.length; i++) {
+			testScreenArr[i].refresh(dt);
+		}
+	}
+}
