75aa374e2bcba3a3a7555a05d695fc1378e9e62b
Jan <Jan@TERMINATOR>
2017-12-27 22:48:03 +0100

    Bug fixes, uncluttered

--- EvoSim/src/TestScreen.java
+++ EvoSim/src/TestScreen.java
@@ -92,9 +92,13 @@ public class TestScreen extends Screen implements TestWrapper{
 			resetView();
 			break;
 		case TEST_ONE_BY_ONE:
-			currentIndex = 0;
+			if (pop.getGen() == 1) {
+				currentIndex = 0;
+			} else {
+				currentIndex = 20;
+			}
 			autoGetNext = true;
-			startSingleTest(pop.getCreatureByID(currentIndex));
+			startSingleTest(pop.getCreatureByIndex(currentIndex));
 			break;
 		default:
 			System.err.println("CONTROL FUNC ENUM ERROR!");
@@ -119,21 +123,20 @@ public class TestScreen extends Screen implements TestWrapper{
 		if (autoRepeat) {
 			Creature tempCret = test.getCreature();
 			test.reset();
-			resetView();
 			enableViewLock();
 			startSingleTest(tempCret);
 		}
 		
 		if (autoGetNext) {
 			test.reset();
-			resetView();
 			enableViewLock();
 			currentIndex ++;
 			if (currentIndex >= pop.getPopulationSize()) {
 				autoGetNext = false;
 				currentIndex = 0;
+				pop.allTested();
 			} else {
-				startSingleTest(pop.getCreatureByID(currentIndex));
+				startSingleTest(pop.getCreatureByIndex(currentIndex));
 			}
 		}
 	}
@@ -202,156 +205,5 @@ public class TestScreen extends Screen implements TestWrapper{
 	public int getCreatureID () {
 		return test.getCreature().getID();
 	}
-
-	// private Test test;
-	// private Population pop = new Population(100);
-	// private float playBackSpeed = 1;
-	// private static float maxPlayBackSpeed = 1024.0f;
-	// private boolean running = false;
-	// private boolean genOver = true;
-	//
-	// public TestScreen(Vec2 gravity_in) {
-	// test = new Test(gravity_in, this);
-	// }
-	//
-	// public void manageTest(double dt) {
-	// if (running)
-	// test.step((float) dt, playBackSpeed);
-	// }
-	//
-	// public void taskDone(int id) {
-	//
-	// if (genOver) {
-	// System.out.println("ID: " + id + " | Fitness:" +
-	// pop.getCreatureByID(id).getFitness());
-	// test.reset();
-	// running = false;
-	// return;
-	// }
-	//
-	// pop.setCurrentFitness(test.getLastFitness());
-	// System.out.println("ID: " + id + " | Fitness:" +
-	// pop.getCurrent().getFitness());
-	//
-	// if (pop.next() == null) {
-	// pop.nextGen();
-	// pop.sortPopulation();
-	// test.reset();
-	// running = false;
-	// genOver = true;
-	// return;
-	// }
-	//
-	// test.reset();
-	// test.setCreature(pop.getCurrent());
-	// test.startTest();
-	//
-	// }
-	// public void manageCommand(ControlFuncTest cf) {
-	// switch (cf) {
-	// case PLAYPAUSE:
-	// running = !running;
-	// if (genOver && running) {
-	// startTest();
-	// }
-	// if (running)
-	// System.out.println("PLAY");
-	// else
-	// System.out.println("PAUSE");
-	// break;
-	// case FAST:
-	// doublePlayBackSpeed();
-	// System.out.println("SPEED: " + playBackSpeed);
-	// break;
-	// case SLOW:
-	// halfPlayBackSpeed();
-	// System.out.println("SPEED: " + playBackSpeed);
-	// break;
-	// case GENERATEPOP:
-	// pop.CreateRandPopulation();
-	// break;
-	// case SHOWBEST:
-	// if (!genOver) break;
-	// startSingleTest(0);
-	// break;
-	// case SHOWMID:
-	// if (!genOver) break;
-	// startSingleTest(49);
-	// break;
-	// case SHOWWORST:
-	// if (!genOver) break;
-	// startSingleTest(99);
-	// break;
-	// default:
-	// System.err.println("CONTROL FUNC ENUM ERROR!");
-	// break;
-	// }
-	// }
-	//
-	// public void startTest() {
-	// if (!pop.isInit()) {
-	// System.err.println("No Population!");
-	// return;
-	// }
-	//
-	// if (genOver && pop.getGen() != 1) {
-	// pop.killPercentage(0.8f);
-	// pop.mutatePop(0.8f);
-	// System.out.println("GENERATION " + pop.getGen());
-	// genOver = false;
-	// }
-	//
-	// genOver = false;
-	// if (test.getCreature() == null) {
-	// test.setCreature(pop.next());
-	// }
-	// test.startTest();
-	// running = true;
-	// }
-	//
-	// public void startSingleTest(int index) {
-	// if (!pop.isInit()) {
-	// System.err.println("No Population!");
-	// return;
-	// }
-	//
-	// if (test.getCreature() == null) {
-	// test.setCreature(pop.get(index));
-	// }
-	// test.startTest();
-	// running = true;
-	// }
-	//
-	// private void halfPlayBackSpeed() {
-	// playBackSpeed *= 0.5f;
-	// }
-	//
-	// private void doublePlayBackSpeed() {
-	// playBackSpeed *= 2;
-	// if (playBackSpeed > maxPlayBackSpeed)
-	// playBackSpeed = maxPlayBackSpeed;
-	// }
-	//
-	// public void setPlayBackSpeed(float speed) {
-	// playBackSpeed = speed;
-	// if (playBackSpeed < maxPlayBackSpeed)
-	// playBackSpeed = maxPlayBackSpeed;
-	// }
-	//
-	// public ArrayList<B2DBody> getWorldInstances() {
-	// return test.getWorldInstances();
-	// }
-	//
-	// public ArrayList<B2DBody> getCreatureInstances() {
-	// return test.getCreatureInstances();
-	// }
-	//
-	// public boolean testIsRunning() {
-	// return running;
-	// }
-	//
-	// public boolean popIsInit() {
-	// return pop.isInit();
-	// }
 
 }
