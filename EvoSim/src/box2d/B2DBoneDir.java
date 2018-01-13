7219f7c928396ff95d2fae949662606052e78c35
Jan <Jan@TERMINATOR>
2018-01-13 19:51:44 +0100

    API Comments B2DBoneDir

--- EvoSim/src/box2d/B2DBoneDir.java
+++ EvoSim/src/box2d/B2DBoneDir.java
@@ -1,5 +1,10 @@
 package box2d;
 
+/**
+ * The {@link B2DBoneDir} enum is used to specify with which end a certain
+ * {@link B2DBone} is connected to a {@link B2DMuscle}.
+ *
+ */
 public enum B2DBoneDir {
 	HEAD, END
 }
