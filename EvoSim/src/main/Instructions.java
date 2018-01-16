ee7805de2015947a40c2514c6a10d3775282d1d2
Jan <Jan@SURFTOP>
2018-01-16 01:59:34 +0100

    Added menu and Listeners

--- EvoSim/src/main/Instructions.java
+++ EvoSim/src/main/Instructions.java
@@ -8,16 +8,20 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import population.Creature;
 
 public class Instructions extends BorderPane {
 	BorderPane[] pages = new BorderPane[3];
+	Label[] titles = new Label[3];
 	int currentPage = 0;
 	
 	private Button btn_next = new Button("Next >");
 	private Button btn_prev = new Button("< Back");
 	private Button btn_showEvo = new Button("Skip >>");
+	
+	private VBox vb_contents = new VBox(10);
 	
 	private final Main main;
 	
@@ -68,6 +72,14 @@ public class Instructions extends BorderPane {
 		updateButtons();
 	}
 	
+	public void setPage(int page) {
+		if (page >= 0 && page < pages.length) {
+			currentPage = page;
+			this.setCenter(pages[currentPage]);
+		}
+		updateButtons();
+	}
+	
 	public void updateButtons() {
 		if (currentPage == 0) {
 			btn_prev.setVisible(false);
@@ -80,12 +92,31 @@ public class Instructions extends BorderPane {
 			btn_prev.setVisible(true);
 			btn_next.setVisible(true);
 		}
+		for (int i = 0; i < pages.length; i++) {
+			titles[i].setFont(new Font(16));
+		}
+		titles[currentPage].setFont(new Font(30));
 	}
 	
 	private void setupPages() {
 		pages[0] = getPage1();
 		pages[1] = getPage2();
 		pages[2] = getPage3();
+		
+		for (int i = 0; i < pages.length; i++) {
+			titles[i] = new Label(pages[i].getId());
+			titles[i].setFont(new Font(20));
+			titles[i].setId(i + "");
+			titles[i].setOnMouseClicked(e -> setPage(Integer.parseInt(((Label) e.getSource()).getId())));
+			vb_contents.getChildren().add(titles[i]);
+		}
+		
+		Layout.defaultMargin(vb_contents);
+		vb_contents.autosize();
+		BorderPane.setAlignment(vb_contents, Pos.CENTER_LEFT);
+		vb_contents.setPrefWidth(200);
+		
+		this.setLeft(vb_contents);
 	}
 
 	private static BorderPane getPage1() {
@@ -108,7 +139,7 @@ public class Instructions extends BorderPane {
 	private static BorderPane getPage2() {
 		BorderPane page = new BorderPane();
 		page.setCenter(new Label("Hi there! \n Page 2"));
-		page.setId("Welcome");
+		page.setId("Algorithmus");
 		return page;
 	}
 
@@ -124,7 +155,7 @@ public class Instructions extends BorderPane {
 		testScreen.showTimer(true);
 		testScreen.setInactiveBackgroundCol(Layout.getSkycolorInactive());
 		page.setCenter(testScreen);
-		page.setId("Welcome");
+		page.setId("Kreaturen");
 		return page;
 	}
 
